91e1e4c6de0ef7dce2e3667d347c502f15285f96
Tarun Eswar <tarun.eswar15@gmail.com>
2022-11-16 21:41:21 -0500

    first commit

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please Enter your name.",
+                    minlength: "Your Name Must Consist of At Least 2 Characters"
+                },
+                subject: {
+                    required: "Please Enter a Valid Subject",
+                    minlength: "Your Subject Must Consist of At Least 4 Characters"
+                },
+                number: {
+                    required: "Please Enter a Valid Number",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Please Fill Out an Email."
+                },
+                message: {
+                    required: "Please Fill out a Message",
+                    minlength: "Please Enter a Valid Message"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
